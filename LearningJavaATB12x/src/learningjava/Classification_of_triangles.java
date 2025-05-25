e81c358abee48e83fd5ff452c794431defa3c287
srava <srava@Sravanthi>
2025-05-25 17:25:09 +0530

    25-05-2025

--- LearningJavaATB12x/src/learningjava/Classification_of_triangles.java
+++ LearningJavaATB12x/src/learningjava/Classification_of_triangles.java
@@ -12,8 +12,9 @@ public class Classification_of_triangles {
 		int L1 = scanner.nextInt();
 		int L2 = scanner.nextInt();
 		int L3 = scanner.nextInt();
-		String result = ((L1==L2)&&(L2==L3)&&(L3==L1)) ? "Equilateral" : (L1==L2)||(L2==L3)||(L3==L1) ? "isosceles" : "scalene";
-		System.out.println("The triangle is "+ result + " triangle.");
+		
+		String result = ((L1+L2>L3 && L3+L1>L2 && L2+L3>L1))? ((L1==L2)&&(L2==L3)&&(L3==L1)) ? "Equilateral triangle" : (L1==L2)||(L2==L3)||(L3==L1) ? "isosceles triangle" : "scalene triangle" : "Not a triangle";
+		System.out.println("This is "+ result);
 		scanner.close();
 
 	}
